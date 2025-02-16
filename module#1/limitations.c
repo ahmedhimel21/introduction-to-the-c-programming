f8a42596d1e86e560166c458edb228cee3d760b1
Ahmed Himel <ahmedhimel000@gmail.com>
2025-02-16 21:04:18 +0600

    setup-and-some-rules

---
+++ module#1/limitations.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+// int main (){
+//     long long int a = 10000000000;
+//     printf("%lld", a);
+//     return 0;
+// }
+
+//float
+#include<stdio.h>
+int main(){
+    double a = 2.123456789;
+    printf("%0.15lf", a);
+    return 0;
+}
---
+++ module#1/variable.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main () {
+    int myAge = 23, dedicatedPer = 10;
+    float avg = 10.5;
+    printf("%d %d %0.1f", myAge, dedicatedPer, avg);
+    return 0;
+}
